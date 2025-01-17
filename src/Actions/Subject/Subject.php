3afe44212b505df8f8cac0792100127e3674a669
Piotr Kurek <piotr.kurek@gmail.com>
2025-01-17 10:28:27 +0100

    update

--- src/Actions/Subject/Subject.php
+++ src/Actions/Subject/Subject.php
@@ -6,13 +6,7 @@ use Workshop\Actions\ActionContract;
 use Workshop\Actions\Traits\WithAttributes;
 use Workshop\Services\Operator\Operator;
 
-class Subject implements ActionContract
+class Subject
 {
-    use WithAttributes;
 
-    public function execute(): int
-    {
-        $service = new Operator();
-        return $service->operation($this->attributes[0]);
-    }
 }
