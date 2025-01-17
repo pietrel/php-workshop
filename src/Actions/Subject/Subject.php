0e672647d4366c306cb44556011a4d3a543278ae
Piotr Kurek <piotr.kurek@gmail.com>
2025-01-17 12:11:33 +0100

    update

--- src/Actions/Subject/Subject.php
+++ src/Actions/Subject/Subject.php
@@ -2,6 +2,15 @@
 
 namespace Workshop\Actions\Subject;
 
-class Subject
+use Workshop\Actions\ActionContract;
+use Workshop\Actions\Traits\WithAttributes;
+
+class Subject implements ActionContract
 {
+    use WithAttributes;
+
+    public function execute(): int
+    {
+        return 1;
+    }
 }
